0539729bf501d622c1a56b04bb4d802fe8463b8c
ilyushatrue <ilya.trukhin.99@mail.ru>
2022-11-10 07:49:36 +1000

    Контейнер сервисов

--- Bookinist/App.xaml.cs
+++ Bookinist/App.xaml.cs
@@ -1,6 +1,35 @@
-﻿namespace Bookinist
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Windows;
+
+namespace Bookinist
 {
     public partial class App
     {
+        private static IHost _host;
+        public static IHost Host => _host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Host.Services;
+
+        internal static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            
+
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var host = Host;
+            base.OnStartup(e);
+            await host.StartAsync();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using var host = Host;
+            base.OnExit(e);
+            await host.StopAsync();
+        }
     }
 }
