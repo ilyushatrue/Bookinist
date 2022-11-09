d7e8c75f21eafb659c8306cda431a4c401a122b2
ilyushatrue <ilya.trukhin.99@mail.ru>
2022-11-10 07:28:58 +1000

    Модель главного окна и локатор моделей-представлений

---
+++ Bookinist/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathCore.Values;
+using MathCore.WPF.ViewModels;
+
+namespace Bookinist.ViewModel
+{
+    internal class MainWindowViewModel : TitledViewModel
+    {
+		private string _title = "Main program window";
+
+		public string Title { get => _title; set => Set(ref _title, value); }
+
+	}
+}
